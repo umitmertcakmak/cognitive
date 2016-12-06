671fbf4276e8f4598c1455daa937802410e446aa
Umit Mert Cakmak <umitmertcakmak@gmail.com>
2016-12-06 15:13:25 +0100

    ...

--- pythonback/setup.py
+++ pythonback/setup.py
@@ -1,11 +1,27 @@
+import os
 from setuptools import setup
 
-setup(name='modelstore',
-      version='0.1',
-      description='Modelstore library',
-      url='https://github.ibm.com/NGP-TWC/git-reposervice/',
-      author='IBM',
-      author_email='ibm@ibm.com',
-      license='IBM',
-      packages=['modelstore', 'modelstore.utils', 'modelstore.conf'],
-      zip_safe=False)
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "modelstore",
+    version = "0.0.4",
+    author = "Umit Mert Cakmak",
+    author_email = "umit.cakmak@pl.ibm.com",
+    description = ("Model save/load to/from git."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['modelstore', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
