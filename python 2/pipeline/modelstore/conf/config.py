1b3bfafdaf3076fb0de7b879468fae1378a9af76
Umit Mert Cakmak <umitmertcakmak@gmail.com>
2016-12-06 13:58:29 +0100

    ...

---
+++ python 2/pipeline/modelstore/conf/config.py
@@ -0,0 +1,13 @@
+git = dict(
+    host_fvt="https://ibm-watson-ml-dev.stage1.mybluemix.net",
+    host_dev="http://nginx-prim-dev.spark.bluemix.net:12501",
+    host_prod="http://nginx-prim-prod.spark.bluemix.net:12501",
+    save="/v1/repos/artifacts",
+    load="/v1/repos/artifacts",
+    branch="master",
+    message="saving model to git",
+    revisionSpec="HEAD"
+)
+
+
+# host_fvt="http://nginx-prim-fvt.spark.bluemix.net:12501"
