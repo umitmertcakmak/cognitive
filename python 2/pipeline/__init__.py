7243c82496413221b604e4c22afb03dabdbd3b6e
Umit Mert Cakmak <umitmertcakmak@gmail.com>
2016-12-06 14:08:52 +0100

    ...

--- python 2/pipeline/__init__.py
+++ python 2/pipeline/__init__.py
@@ -3,6 +3,10 @@ from IBMSparkPipeline import IBMSparkPipeline
 from IBMSparkPipelineModel import IBMSparkPipelineModel
 from Source import Source
 from Sink import Sink
+from api_client import ApiClient
+from config import git
+from model_store import ModelStore
+from utils import Utils
 
 # Set the version of module
 __version__ =  '0.1.347'
--- python 2/pipeline/modelstore/__init__.py
+++
@@ -1,4 +0,0 @@
-from .model_store import ModelStore
-from .api_client import ApiClient
-from .utils.utils import Utils
-
