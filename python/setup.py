6089b0913dc993c22c5c6ce1a92fce6840f58cc5
Umit Mert Cakmak <umitmertcakmak@gmail.com>
2016-12-06 13:55:18 +0100

    ...

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='ibm-modelstore',
+setup(name='modelstore',
       version='0.1',
       description='Modelstore library',
-      url='https://github.ibm.com/NGP-TWC/platform-pipeline/',
+      url='https://github.ibm.com/NGP-TWC/git-reposervice/',
       author='IBM',
       author_email='ibm@ibm.com',
       license='IBM',
